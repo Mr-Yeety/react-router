76084e99ae2cf39c2aab066e5e3877b4a18105e1
Mr-Yeety <98505470+Mr-Yeety@users.noreply.github.com>
2023-08-29 08:21:58 -0600

    React Router Tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/pricing" element={<Pricing/>}/>
+      </Routes>
+
+
+      <Routes>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
